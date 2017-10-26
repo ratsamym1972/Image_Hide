48676e6faf8b44fc253020c35393f493b208a635
Ratsamy Maokhamphiou <ratsamym@gmail.com>
2017-10-25 23:05:11 -0700

    files commit

---
+++ Image_Hide/js/image_hide.js
@@ -0,0 +1,14 @@
+$(window).load(function(){
+  $("#handle").click(toggleImage);
+});
+
+function toggleImage(){
+  if($("#handle").html() == '+'){
+    $("#photo").show(1000, function(){$("#footer").show();});
+    $("#handle").html('-');
+  }else{
+    $("#footer").hide();
+    $("#photo").hide(1000);
+    $("#handle").html('+');
+  }
+}
